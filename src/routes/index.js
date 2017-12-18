f11fd30716fafbcbb5b1ebf94ed24df061292f6b
liuzfd <liuzfd@yonyou.com>
2017-12-18 10:01:12 +0800

    更新demo

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,13 +9,13 @@ import App from '../App'; //主应用
 // import ListDemo from '../../dist/component/list/demo/ListDemo'
 // import SS from '../../lib/component/button/demo/YYButtonDemo'
 import YYList from '../../lib/component/list/demo/YYListDemo'
-import Refer from '../../lib/component/refer/YYReferlist'
+// import Refer from '../../lib/component/refer/YYReferlist'
 import Referdemo from '../../lib/component/refer/demo/YYreferDemo'
 // import Button from '../../dist/component/button/SSButton'
 export default
     <Route path="/" component={App}>
         <IndexRoute key='001' component={YYList}></IndexRoute>
-        <Route path="refer" component={Refer}/>
+        <Route path="listview" component={YYList}/>
         <Route path="referdemo" component={Referdemo}/>
        {/* <IndexRoute key='001' component={ImagePickerDemo}></IndexRoute>
         <Route path="listDemo" component={ListDemo}/>*/}
